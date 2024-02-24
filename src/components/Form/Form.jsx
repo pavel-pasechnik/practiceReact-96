db9ed9dd9242929a92201e826248fbb32229df8c
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-02-24 19:27:11 +0200

    controled form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,42 +1,112 @@
+// export default function Form({ onSubmit }) {
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     const { name, email, street, city } = e.target.elements;
+
+//     const newUser = {
+//       name: name.value,
+//       email: email.value,
+//       address: {
+//         street: street.value,
+//         city: city.value,
+//       },
+//     };
+
+//     onSubmit(newUser);
+
+//     e.target.reset();
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor="name">
+//         Name
+//         <input type="text" name="name" />
+//         <br />
+//       </label>
+//       <label htmlFor="email">
+//         Email
+//         <input type="text" name="email" />
+//       </label>
+//       <br />
+//       <label htmlFor="street">
+//         Street
+//         <input type="text" name="street" />
+//       </label>
+//       <br />
+//       <label htmlFor="city">
+//         City
+//         <input type="text" name="city" />
+//       </label>
+//       <br />
+//       <button type="submit">Send</button>
+//     </form>
+//   );
+// }
+import { useState } from "react";
+
 export default function Form({ onSubmit }) {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [street, setStreet] = useState("");
+
+  const handleChange = ({ target: { value, name} }) => {
+    if (name === "name") {
+      setName(value); 
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "city") {
+      setCity(value);
+    } else if (name === "street") {
+      setStreet(value);
+    } 
+
+
+  }
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const { name, email, street, city } = e.target.elements;
-
     const newUser = {
-      name: name.value,
-      email: email.value,
+      name,
+      email,
       address: {
-        street: street.value,
-        city: city.value,
+        street,
+        city
       },
     };
 
     onSubmit(newUser);
 
-    e.target.reset();
+    setName("");
+    setEmail("");
+    setCity("");
+    setStreet("");
   };
+
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">
+      <label>
         Name
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleChange} />
         <br />
       </label>
-      <label htmlFor="email">
+      <label>
         Email
-        <input type="text" name="email" />
+        <input type="text" name="email" value={email} onChange={handleChange} />
       </label>
       <br />
-      <label htmlFor="street">
+      <label>
         Street
-        <input type="text" name="street" />
+        <input type="text" name="street" value={street} onChange={handleChange} />
       </label>
       <br />
-      <label htmlFor="city">
+      <label>
         City
-        <input type="text" name="city" />
+        <input type="text" name="city" value={city} onChange={handleChange} />
       </label>
       <br />
       <button type="submit">Send</button>
